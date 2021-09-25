7c0256ded86c33540344acab59cb70ec623e21f8
JoaoZati <joaozati@gmail.com>
2021-09-25 19:59:04 -0300

    Criado o frontend dos botões:
    def create_digit_bottons

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,6 +4,8 @@ LARGE_FONT_STYLE = ('Arial', 40, 'bold')
 SMALL_FONT_STYLE = ("Arial", 16)
 LIGHT_GRAY = '#F5F5F5'
 LABEL_COLOR = '#25265E'
+BUTTON_BG = '#CAF0F8'
+DIGIT_FONT_STYLE = ('Arial', 24, 'bold')
 
 
 class Calculator:
@@ -21,6 +23,20 @@ class Calculator:
 
         self.buttons_frame = self.create_buttons_frame()
 
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            '.': (4, 1), 0: (4, 2),
+        }
+
+        self.create_digit_bottons()
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill='both')
+        return frame
+
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E,
                                bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
@@ -32,15 +48,16 @@ class Calculator:
 
         return total_label, label
 
-    def create_display_frame(self):
-        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
-        frame.pack(expand=True, fill='both')
-        return frame
-
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill='both')
         return frame
+
+    def create_digit_bottons(self):
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit),
+                               bg=BUTTON_BG, fg=LABEL_COLOR, font=DIGIT_FONT_STYLE, borderwidth=0)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)  # NSEW fill the entire cell
 
     def run(self):
         self.window.mainloop()
